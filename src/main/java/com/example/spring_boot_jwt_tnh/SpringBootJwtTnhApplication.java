c6d8cd6d69295a13de937049408ce6ee2fffb628
hien123454 <ngochien15092000@gmail.com>
2021-09-22 10:34:25 +0700

    commit lan 1

---
+++ src/main/java/com/example/spring_boot_jwt_tnh/SpringBootJwtTnhApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_tnh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTnhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtTnhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_jwt_tnh/SpringBootJwtTnhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_tnh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtTnhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
